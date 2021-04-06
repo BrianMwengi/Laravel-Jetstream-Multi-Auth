37b61d7069416a30e8692ca5b0411fd4b77434dd
unknown <mwengibrian@gmail.com>
2021-04-06 09:03:00 +0300

    Updated Title & Code Base

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=<, initial-scale=1.0">
-    <title>Laravel 8 Jetstream Mult Auth</title>
+    <title>Laravel 8 Jetstream Multi Auth</title>
 </head>
 <body>
     <p style ="text-align:center">Admin Dashboard.</p>
